9926d0d954eeaaaec9fc5ce03d640b4a78c9ebec
Steven Vicky <70185865+VigneshOfficial2020@users.noreply.github.com>
2021-07-24 20:27:00 +0530

    typescript basic execution

---
+++ main.js
@@ -0,0 +1,5 @@
+function display(message) {
+    console.log(message);
+}
+var message = 'Hi Hello';
+display(message);
---
+++ main.ts
@@ -0,0 +1,7 @@
+function display(message:String) {
+    console.log(message);
+    
+}
+
+let message = 'Hi Hello'
+display(message)
